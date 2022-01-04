fd7d6fea7a15a927e29034453aabd0067ee8b3cb
Aryan <aryangodara03@gmail.com>
2022-01-04 21:50:30 +0530

    new content added

--- WEBD/chapter_5/sandbox.js
+++ WEBD/chapter_5/sandbox.js
@@ -27,13 +27,21 @@ name: 'crystal',
 age: 30,
 email: 'crystal@ninja.com',
 location: 'berlin',
-blogs: ['why mac & cheese rules', '10 things to make with lego'],
+blogs:  [     // Each element of this list, is an 'object'
+    {title: 'why mac and cheese rules', likes: 30 },
+    {title: '10 things to make with marmite', likes: 30}
+],
 
 // Adding a method to the object
 login: function(){console.log('user logged in...')},
 
 logBlogs: function(){
-        console.log(this.blogs);
+        // console.log(this.blogs);
+        console.log("This user has written the following blogs:-");
+        this.blogs.forEach( (blog) => {
+            console.log(blog);
+            console.log(blog.title, blog.likes);
+        })
     },
 callObject: function() { console.log(this) },
 };
@@ -42,3 +50,47 @@ user.login();      //TODO Kind of like using a string method lol
 user.logBlogs();
 user.callObject();
 console.log(this);  //! The GLOBAL THIS refers to the 'window object' in Javascript
+
+
+//? Objects in Arrays
+
+console.log("\n\n");
+
+const blogs = [     // Each element of this list, is an 'object'
+    {title: 'why mac and cheese rules', likes: 30 },
+    {title: '10 things to make with marmite', likes: 30}
+];
+
+console.log(blogs) ;
+
+
+//? Math Object
+console.log("\n\n");
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.e);
+console.log();
+
+
+//? Primitive vs Reference Types
+
+/*
+
+! Primitive Types
+* numbers
+* strings
+* booleans
+* null
+* undefined
+* symbols
+
+! Reference Types
+* all types of objects
+    * obejct literals
+    * arrays
+    * functions
+    * dates
+    * all other objects
+
+*/
